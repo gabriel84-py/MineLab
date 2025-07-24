9a91187b99ba2abf7ad0e74f139d4054b1fcb924
Linkxx08 <gaspard.portefaix@gmail.com>
2025-07-24 09:40:31 +0200

    Ajout des dossiers des processus de sécurités des plugins spigot, organisation des dossiers

---
+++ security_template/plugin/spigot-1-21x/spigot-1-21-3/security/Servermac.java
@@ -0,0 +1,46 @@
+package org.nexko.mineLabSecurityTemplate.security;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.util.Enumeration;
+
+public class Servermac {
+
+    public String getMacAddress() {
+        try {
+            InetAddress ip = InetAddress.getLocalHost();
+            NetworkInterface network = NetworkInterface.getByInetAddress(ip);
+
+            if (network == null) {
+                // Si on n'a pas trouvé l'interface par l'IP locale, on parcourt toutes les interfaces
+                Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+                while (interfaces.hasMoreElements()) {
+                    NetworkInterface ni = interfaces.nextElement();
+                    if (!ni.isLoopback() && ni.getHardwareAddress() != null) {
+                        network = ni;
+                        break;
+                    }
+                }
+            }
+
+            if (network == null) {
+                return "Interface réseau non trouvée.";
+            }
+
+            byte[] mac = network.getHardwareAddress();
+            if (mac == null) {
+                return "Adresse MAC non disponible.";
+            }
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < mac.length; i++) {
+                sb.append(String.format("%02X%s", mac[i], (i < mac.length - 1) ? "-" : ""));
+            }
+
+            return sb.toString();
+
+        } catch (Exception e) {
+            return "Erreur : " + e.getMessage();
+        }
+    }
+}
