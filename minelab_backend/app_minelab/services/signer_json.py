1ea2bdf6e754b853876d44b8f9ee0e944a3810a8
gabriel84-py <hexahack.g8430@gmail.com>
2025-07-30 15:26:45 +0200

    okay verifier

--- minelab_backend/app_minelab/services/signer_json.py
+++ minelab_backend/app_minelab/services/signer_json.py
@@ -2,17 +2,19 @@ import rsa
 import json
 import base64
 
-def signer_json(data_dict: dict):
-    with open("/Users/gabrieljeanvermeille/PycharmProjects/MineLab/private.pem", "rb") as f:
-        privkey = rsa.PrivateKey.load_pkcs1(f.read())
+def verifier_signature(response: dict) -> bool:
+    with open("/Users/gabrieljeanvermeille/PycharmProjects/MineLab/public.pem", "rb") as f:
+        pubkey = rsa.PublicKey.load_pkcs1(f.read())
 
-    message = json.dumps(data_dict).encode()
-    signature = rsa.sign(message, privkey, 'SHA-256')
+    # CORRECTIF : on décode le message de base64 vers bytes
+    message = base64.b64decode(response["message"])
+    signature = base64.b64decode(response["signature"])
 
-    return {
-        "message": base64.b64encode(message).decode(),
-        "signature": base64.b64encode(signature).decode()
-    }
+    try:
+        rsa.verify(message, signature, pubkey)
+        return True  # signature OK
+    except rsa.VerificationError:
+        return False  # signature invalide
 
 
 
