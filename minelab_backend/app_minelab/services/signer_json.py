c627c0a0aaa7a171bbf48942f0bf11b77f83fd58
gabriel84-py <hexahack.g8430@gmail.com>
2025-07-30 15:27:22 +0200

    okay verifier

--- minelab_backend/app_minelab/services/signer_json.py
+++ minelab_backend/app_minelab/services/signer_json.py
@@ -2,11 +2,26 @@ import rsa
 import json
 import base64
 
+def signer_json(data_dict: dict):
+    with open("/Users/gabrieljeanvermeille/PycharmProjects/MineLab/private.pem", "rb") as f:
+        privkey = rsa.PrivateKey.load_pkcs1(f.read())
+
+    message = json.dumps(data_dict).encode()
+    signature = rsa.sign(message, privkey, 'SHA-256')
+
+    return {
+        "message": base64.b64encode(message).decode(),
+        "signature": base64.b64encode(signature).decode()
+    }
+
+
+
+
 def verifier_signature(response: dict) -> bool:
     with open("/Users/gabrieljeanvermeille/PycharmProjects/MineLab/public.pem", "rb") as f:
         pubkey = rsa.PublicKey.load_pkcs1(f.read())
 
-    # CORRECTIF : on décode le message de base64 vers bytes
+    #CORRECTIF : on décode le message de base64 vers bytes
     message = base64.b64decode(response["message"])
     signature = base64.b64decode(response["signature"])
 
@@ -16,21 +31,6 @@ def verifier_signature(response: dict) -> bool:
     except rsa.VerificationError:
         return False  # signature invalide
 
-
-
-
-def verifier_signature(response: dict) -> bool:
-    with open("/Users/gabrieljeanvermeille/PycharmProjects/MineLab/public.pem", "rb") as f:
-        pubkey = rsa.PublicKey.load_pkcs1(f.read())
-
-    message = response["message"].encode()
-    signature = base64.b64decode(response["signature"])
-
-    try:
-        rsa.verify(message, signature, pubkey)
-        return True  # signature OK
-    except rsa.VerificationError:
-        return False  # signature invalide
 
 
 if __name__ == "__main__":
