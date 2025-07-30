5259687190e4be92d2035bc6fdebe187eca6f2e7
gabriel84-py <hexahack.g8430@gmail.com>
2025-07-30 16:55:41 +0200

    new func

---
+++ minelab_backend/app_minelab/services/changer_license_taille.py
@@ -0,0 +1,20 @@
+from minelab_backend.app_minelab.database import Base, engine, SessionLocal
+from minelab_backend.app_minelab.models.license import License
+
+def changer_taille(id_sent: int, taille_sent: int):
+    Base.metadata.create_all(bind=engine)
+    db = SessionLocal()
+
+    license_obj = db.query(License).filter(License.id == id_sent).first()
+
+    # Étape 3 : Vérifier si elle existe
+    if license_obj:
+        # Étape 4 : Modifier la valeur de size
+        license_obj.size = taille_sent  # ← nouvelle valeur
+        db.commit()
+        db.refresh(license_obj)
+        print("Taille mise à jour :", license_obj.size)
+    else:
+        print("Licence non trouvée")
+
+    db.close()
