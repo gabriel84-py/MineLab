e545a7881dfec444f25c9eb951dd5be6ef908f34
gabriel84-py <hexahack.g8430@gmail.com>
2025-07-30 10:57:17 +0200

    .pem are here !!!

--- minelab_backend/app_minelab/database.py
+++ minelab_backend/app_minelab/database.py
@@ -3,10 +3,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-DB_DIR = os.path.join(BASE_DIR, "minelab_backend")
+DB_DIR = os.path.join(BASE_DIR, "db_backend")
 os.makedirs(DB_DIR, exist_ok=True)  # <-- crée le dossier s'il n'existe pas
 
-DATABASE_URL = "sqlite:////Users/gabrieljeanvermeille/PycharmProjects/MineLab/minelab_backend/app_minelab/minelab_backend/minelab.db"
+DATABASE_URL = "sqlite:////Users/gabrieljeanvermeille/PycharmProjects/MineLab/db_backend/app_minelab/db_backend/minelab.db"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
